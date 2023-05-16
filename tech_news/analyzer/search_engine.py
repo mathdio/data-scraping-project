c597fbfe8d135ac0b9bb00891ce9e1baf8c8911f
Matheus Diógenes <mathdiogenes@gmail.com>
2023-05-16 20:34:32 -0300

    Req 08 em andamento

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 from tech_news.database import search_news
 import re
+from datetime import datetime
 
 # Requisito 7
 
@@ -14,7 +15,14 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    # if not datetime.fromisoformat(date):
+    #     raise ValueError()
+    valid_date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+    filtered = [
+        (news["title"], news["url"])
+        for news in search_news({"timestamp": valid_date})
+    ]
+    return filtered
 
 
 # Requisito 9
@@ -23,3 +31,4 @@ def search_by_category(category):
 
 
 # print(search_by_title("site responsivo:"))
+# print(search_by_date("2023-05-10"))
